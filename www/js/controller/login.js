799659a8877dc099404177e997e5e1e193db45fe
yjesefcu <l00164778@sina.com>
2018-11-21 14:50:46 +0800

    平台查询接口改成信发自己的ip

--- www/js/controller/login.js
+++ www/js/controller/login.js
@@ -5,7 +5,7 @@
  */
 
 var gPublicApiHost = 'http://47.104.75.86:8090';        // 公有云接口
-// var gPublicApiHost = 'http://47.97.167.195:8090';        // 因泰来接口
+// var gPublicApiHost = 'http://222.175.5.94:8090';        // 信发
 
 // var gPublicApiHost = 'http://114.215.90.83:8090';
 
