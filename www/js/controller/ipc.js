457ecefd3f358d38a612617a33295c06566f80d8
yjesefcu <l00164778@sina.com>
2019-01-04 11:45:54 +0800

    视频监控

---
+++ www/js/controller/ipc.js
@@ -0,0 +1,255 @@
+app.config(['$sceDelegateProvider',function($sceDelegateProvider) {
+    $sceDelegateProvider.resourceUrlWhitelist([
+        'self',
+        'http://114.215.90.83:10810/**'
+    ]);
+}]);
+
+function onVideoPause(ipcId) {       // 暂停视频播放
+    var scope = angular.element("#videoMonitor").scope();
+    if (scope) {
+        scope.pausePlay(parseInt(ipcId));
+    }
+}
+
+app.controller('VideoMonitorCtrl', function ($scope, $timeout, platformService, ajax) {
+    $scope.sn = GetQueryString("sn");
+    $scope.ipcs = [];
+    $scope.isLoading = false;
+    $scope.loadingFailed = false;
+    $scope.listType = 'row';
+
+    $scope.getIpcList = function() {
+        $scope.isLoading = true;
+        $scope.loadingFailed = false;
+        ajax.get({
+            url: platformService.getIpcServiceHost() + '/ipc/findByStationSn',
+            data: {
+                sn: $scope.sn,
+                sync: true
+            },
+            headers: {
+                Accept: 'application/json'
+            },
+            success: function (data) {
+                $scope.isLoading = false;
+                data.forEach(function (ipc) {
+                    if (ipc.status === '0') {
+                        ipc.statusName = '在线';
+                        ipc.online = true;
+                    } else if (ipc.status) {
+                        ipc.statusName = '不在线';
+                        ipc.online = false;
+                    } else {
+                        ipc.statusName = '';
+                        ipc.online = true;
+                    }
+                    if (ipc.type === 'YS7') {
+                        ipc.streamUrl = '/templates/video-monitor/iframe/ys7-player.html?url=' + ipc.liveAddress + '&autoplay&ipcId=' + ipc.id;
+                    } else {
+                        // 从liveAddress中解析出flv视频地址
+                        var liveAddress = ipc.liveAddress;
+                        var serviceHost = liveAddress.substring(0, liveAddress.indexOf(':', 5)),
+                            deviceId=GetQueryString("device", liveAddress),
+                            channelId=GetQueryString("channel", liveAddress);
+                        var streamUrl = serviceHost + ':10812/nvc/' + deviceId + '/flv/hls/stream_' + channelId + '.flv';
+                        ipc.streamUrl = '/templates/video-monitor/iframe/kk-player.html?url=' + streamUrl + '&ipcId=' + ipc.id;
+                    }
+                });
+                $scope.ipcs = data;
+                $scope.$apply();
+            },
+            error: function () {
+                $scope.loadingFailed = true;
+                $scope.isLoading = false;
+                $.notify.error('获取摄像头列表失败');
+                $scope.$apply();
+            }
+        });
+    };
+
+    $scope.startPlay = function (ipc) {
+        ipc.startPlay = true;
+    };
+
+    $scope.pausePlay = function (ipcId) {
+        $scope.ipcs.forEach(function (ipc) {
+            if (ipc.id === ipcId) {
+                ipc.startPlay = false;
+                return false;
+            }
+        });
+        $scope.$apply();
+    };
+
+    $scope.switchListType = function () {
+        $scope.listType = $scope.listType === 'col' ? 'row' : 'col';
+    };
+
+    $scope.openSnalModal = function () {
+
+    };
+
+    $timeout($scope.getIpcList, 500);
+});
+
+app.controller('IpcRecordHistoryCtrl', function ($scope, platformService, $timeout, ajax, routerService) {
+    $scope.sn = GetQueryString("sn");
+    $scope.records = [];
+    $scope.isLoading = false;
+    var isSavingRecords = [];
+    var refreshInterval = null;
+
+    $scope.getRecords = function () {
+        $scope.isLoading = true;
+        ajax.get({
+            url: platformService.getIpcServiceHost() + '/captureRecord/findByStationSn',
+            data: {
+                sn: $scope.sn,
+                type: 'PIC'
+            },
+            headers: {
+                Accept: "application/json; charset=utf-8"
+            },
+            success: function (data) {
+                $scope.isLoading = false;
+                isSavingRecords = [];
+                data.forEach(function (item) {
+                   var createTime = item.createTime;
+                   item.createTime = createTime.substring(0, 4) + '年' + parseInt(createTime.substring(5, 7)) + '月' +
+                       parseInt(createTime.substring(8, 10)) + '日 ' + createTime.substring(11, 19);
+                   if (item.status !== '0') {
+                       isSavingRecords.push(item.id);
+                   }
+                });
+                $scope.records = data.reverse();
+                $scope.$apply();
+                startRefreshInterval();
+            },
+            error: function () {
+                $.notify.error('获取巡检记录失败');
+                $scope.isLoading = false;
+                $scope.$apply();
+            }
+        });
+    };
+
+    function startRefreshInterval() {
+        if (isSavingRecords.length && refreshInterval === null) {
+            refreshInterval = setInterval(function () {
+                if (!isSavingRecords.length) {
+                    clearRefreshInterval();
+                    return;
+                }
+                isSavingRecords.forEach(function (recordId) {
+                    refreshRecordStatus(recordId);
+                });
+            }, 3000);
+        }
+    }
+
+    function clearRefreshInterval() {
+        if (refreshInterval !== null) {
+            clearInterval(refreshInterval);
+            refreshInterval = null;
+        }
+    }
+
+    function refreshRecordStatus(recordId) {
+        ajax.get({
+            url: platformService.getIpcServiceHost() + '/captureRecord/' + recordId,
+            headers: {
+                Accept: "application/json; charset=utf-8"
+            },
+            success: function (data) {
+                $scope.records.forEach(function (record) {
+                    if (record.id === data.id) {
+                        $.extend(record, {
+                            status: data.status,
+                            downCount: data.downCount,
+                            allCount: data.allCount
+                        });
+                        if (data.status === '0') {
+                            isSavingRecords.splice(isSavingRecords.indexOf(recordId), 1);
+                        }
+                        return false;
+                    }
+                });
+                $scope.$apply();
+            }
+        });
+    }
+
+    $scope.openRecordDetail = function (record) {
+        routerService.openPage($scope, '/templates/video-monitor/ipc-pic-record-detail.html', {
+            record: record
+        });
+    };
+
+    $scope.$on('$destroy', function (event) {
+       clearRefreshInterval();
+    });
+
+    $timeout($scope.getRecords, 500);
+});
+
+app.controller('IpcPicRecordDetailCtrl', function ($scope, platformService, ajax, $timeout, routerService) {
+    var createMoment = moment($scope.record.createTime, 'YYYY-MM-DD HH:mm:ss');
+    var dayOfWeek = createMoment.format('e');
+    var daysOfWeek = ['日', '一', '二', '三', '四', '五', '六'];
+    $scope.createTime = createMoment.format('M月D日 ') + '周' + daysOfWeek[dayOfWeek];
+    $scope.images = [];
+    var refreshInterval = null;
+
+    function getRecordDetail() {
+        ajax.get({
+            url: platformService.getIpcServiceHost() + '/captureRecord/' + $scope.record.id,
+            headers: {
+                Accept: "application/json; charset=utf-8"
+            },
+            success: function (data) {
+                $scope.images = [];
+                data.ipcRecord.forEach(function (ipc) {
+                    $scope.images.push(platformService.getIpcServiceHost() + ipc.srcLink);
+                });
+                $.extend($scope.record, {
+                    status: data.status,
+                    allCount: data.allCount,
+                    downCount: data.downCount
+                });
+                $scope.$apply();
+                if (data.status === '0') {
+                    clearRefreshInterval();
+                } else {
+                    startRefreshInterval();
+                }
+            }
+        });
+    }
+
+    $scope.openGallery = function (index) {
+        routerService.openPage($scope, '/templates/base-gallery.html', {
+            images: $scope.images,
+            index: index + 1
+        });
+    };
+
+    function startRefreshInterval() {
+        if (refreshInterval === null) {
+            refreshInterval = setInterval(getRecordDetail, 3000);
+        }
+    }
+
+    function clearRefreshInterval() {
+        if (refreshInterval !== null) {
+            clearInterval(refreshInterval);
+            refreshInterval = null;
+        }
+    }
+
+    $scope.$on('$destroy', function (event) {
+        clearRefreshInterval();
+    });
+
+    $timeout(getRecordDetail, 500);
+});
