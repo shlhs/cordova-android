e6463fbb3658ad7cb7d7ee9559b9a34b2de8d856
yjesefcu <l00164778@sina.com>
2019-01-09 17:04:26 +0800

    解决抄表数据过长时显示不全且无法左移的问题

--- www/js/controller/energy.js
+++ www/js/controller/energy.js
@@ -366,6 +366,7 @@ app.controller('EnergyMeterReadingCtrl', function ($scope, ajax, $compile, platf
         $scope.tableHeader = [$scope.currentLabel.name+'名称', $scope.startDate.substring(0, 16), $scope.endDate.substring(0, 16), '差值', '单位'];
         $scope.tableBodyData = [];
         if (!sns.length) {
+            refreshTable();
             return;
         }
         ajax.get({
@@ -408,6 +409,7 @@ app.controller('EnergyMeterReadingCtrl', function ($scope, ajax, $compile, platf
                     $scope.tableBodyData.push(rowData);
                 });
                 $scope.$apply();
+                refreshTable();
             },
             error: function () {
                 $.notify.error('获取数据失败');
@@ -415,8 +417,64 @@ app.controller('EnergyMeterReadingCtrl', function ($scope, ajax, $compile, platf
         });
     }
 
+    function refreshTable() {
+        var table = $("#meterReadingTable").DataTable();
+        if (table) {
+            table.destroy(true);
+            table = null;
+            var html = "<energy-meter-reading-table table-header='tableHeader' table-body-data='tableBodyData'></energy-meter-reading-table>";
+            var compileFn = $compile(html);
+            var $dom = compileFn($scope);
+            // 添加到文档中
+            $dom.appendTo($('#tableParent'));
+        }
+    }
+
     // initDatePicker();
     getConfig(stationSn);
+});
+
+app.directive('energyMeterReadingTable', [function(){
+    return {
+        restrict: 'E',
+        templateUrl: '/templates/energy/energy-meter-reading-table-template.html',
+        replace: true,
+        scope: {
+            tableHeader: '=',
+            tableBodyData: '='
+        },
+        controller: ['$scope', '$attrs', function($scope, $attrs){
+            console.log('finish');
+        }]
+    };
+}]);
+
+
+app.directive('energyMeterReadingTableRepeatFinish',function(){
+    return {
+        link: function(scope,element,attr){
+            if(scope.$last == true){
+                setTimeout(function () {
+                    $.fn.dataTable.ext.errMode = 'none'; //不显示任何错误信息
+                    var height = screen.height - 210;
+                    var table = $('#meterReadingTable').DataTable( {
+                        searching: false,
+                        ordering: false,
+                        scrollY:        height + 'px',
+                        scrollX:        true,
+                        scrollCollapse: true,
+                        paging:         false,
+                        info: false,
+                        fixedRows: false,
+                        fixedColumns: {
+                            leftColumns: 1
+                        }
+                    } );
+                    // scope.$parent.table = table;
+                }, 100);
+            }
+        }
+    }
 });
 
 app.controller('EnergyReportCtrl', function ($scope, ajax, $compile, platformService) {
