a7895de4da099aed86f5310493d9d12100135a3b
yjesefcu <l00164778@sina.com>
2019-03-01 11:04:43 +0800

    用能统计支路用能趋势中昨日、上月、去年都显示全部都数据

--- www/js/controller/energy.js
+++ www/js/controller/energy.js
@@ -1350,9 +1350,9 @@ app.controller('EnergyOverviewZhiluCtrl', function ($scope, ajax, platformServic
             var times = [];
             var currentMoment = moment().format('YYYY-MM-DD HH:mm:ss.000');
             data.now.time_keys.forEach(function (t) {
-                if (t > currentMoment) {
-                    return false;
-                }
+                // if (t > currentMoment) {
+                //     return false;
+                // }
                switch ($scope.timeType) {
                    case '日':
                        times.push(parseInt(t.substring(11, 13)) + '时');
@@ -1361,7 +1361,7 @@ app.controller('EnergyOverviewZhiluCtrl', function ($scope, ajax, platformServic
                        times.push(parseInt(t.substring(8, 10)) + '日');
                        break;
                    case '年':
-                       times.push(parseInt(t.substring(5, 7) + '月'));
+                       times.push(parseInt(t.substring(5, 7)) + '月');
                        break;
                }
             });
@@ -1397,14 +1397,14 @@ app.controller('EnergyOverviewZhiluCtrl', function ($scope, ajax, platformServic
                 xAxis: {
                     type: 'category',
                     boundaryGap: [0, 0.01],
-                    name: 'kWh',
                     nameLocation: 'start',
                     nameGap: '5',
                     data: times,
                 },
                 yAxis: {
+                    name: 'kWh',
                     type: 'value',
-                    nameGap: '5',
+                    nameGap: 20,
                     nameLocation: 'start',
                     nameTextStyle: {
                         verticalAlign: 'top'
