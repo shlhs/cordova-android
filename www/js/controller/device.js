7a1101afeed08f45fffd26c94778107bc052c0d4
yjesefcu <l00164778@sina.com>
2019-03-29 11:07:43 +0800

    控制中心不显示模拟量，对接cloud的下发接口

--- www/js/controller/device.js
+++ www/js/controller/device.js
@@ -837,7 +837,8 @@ app.controller('DeviceRemoteControlCtrl', function ($scope, $interval, routerSer
         ajax.get({
             url: platformService.getDeviceMgmtHost() + '/management/devices/' + $scope.device_sn + '/variables',
             data: {
-                rw: 3
+                rw: 3,
+                type: 'Digital'
             },
             success: function (response) {
                 $scope.isLoading = false;
@@ -997,9 +998,9 @@ app.controller('DeviceRemoteControlConfirmCtrl', function ($scope, ajax, platfor
         var controlObj = $scope.controlObj;
         $scope.isSubmitting = true;
         ajax.post({
-            url: platformService.getDeviceMgmtHost() + '/notification/value_changed',
+            url: '/varcontrol/action',
             data: {
-                type: controlObj.action,
+                type: controlObj.action === 'yaokong-act' ? 1 : 2,
                 variable_sn: controlObj.sn,
                 variable_value: controlObj.type === 'Digital' ? controlObj.value : newValue,
                 pwd: pwd
@@ -1009,9 +1010,13 @@ app.controller('DeviceRemoteControlConfirmCtrl', function ($scope, ajax, platfor
                     if (response.code === 200) {
                         $.notify.info('成功下发');
                         $scope.onCancel();
-                    } else {
+                    } else if (response.code === 403) {
                         $scope.isSubmitting = false;
                         $scope.error = '密码错误';
+                        $scope.$apply();
+                    } else {
+                        $scope.isSubmitting = false;
+                        $scope.error = '下发失败';
                         $scope.$apply();
                     }
                 } else {
