8b46ba43bb8facdd64686059a466438a137d7ab2
yjesefcu <l00164778@sina.com>
2019-04-15 20:40:54 +0800

    合必达

--- www/js/controller/login.js
+++ www/js/controller/login.js
@@ -4,8 +4,8 @@
  * Created by liucaiyun on 2017/7/23.
  */
 
-var gPublicApiHost = 'http://47.104.75.86:8090';        // 公有云接口
-// var gPublicApiHost = 'http://47.105.143.250:8090';        // 因泰来接口
+// var gPublicApiHost = 'http://47.104.75.86:8090';        // 公有云接口
+var gPublicApiHost = 'http://47.105.216.220:8090';        // 合必达接口
 
 app.controller('LoginCtrl', function ($scope, $timeout, platformService, userService, $state, $http, ajax) {
     $scope.error = '';
