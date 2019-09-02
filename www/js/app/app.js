9db8fa74d61ccf93c46d6cab45ab95f8847698f1
yjesefcu <l00164778@sina.com>
2019-09-02 20:51:11 +0800

    html的css压缩写法

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -5,7 +5,7 @@
 var app = angular.module('myApp', ['ngAnimate', 'ui.router', 'ui.router.state.events']);
 var loginExpireCheckEnable = false;       // 是否检查鉴权过期
 var defaultPlatIpAddr = "";     // 平台默认ip，格式为：http://118.190.51.135
-var gShowEnergyPage = true;     // 是否显示能效页面，不显示能效页面时运维人员会看到抢单页面
+var gShowEnergyPage = false;     // 是否显示能效页面，不显示能效页面时运维人员会看到抢单页面
 
 app.run(['$animate', function ($animate) {
     $animate.enabled(true);
