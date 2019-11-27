a9bd5f15dd70ca7415116e4c741a7478e1a82812
yjesefcu <l00164778@sina.com>
2019-11-27 10:14:09 +0800

    国电：IP改为：39.107.141.230

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -4,7 +4,7 @@
  */
 var app = angular.module('myApp', ['ngAnimate', 'ui.router', 'ui.router.state.events']);
 var loginExpireCheckEnable = false;       // 是否检查鉴权过期
-var defaultPlatIpAddr = "http://39.104.74.199";     // 平台默认ip，格式为：http://118.190.51.135
+var defaultPlatIpAddr = "http://39.107.141.230";     // 国电瑞恒科
 var gShowEnergyPage = false;     // 是否显示能效页面，不显示能效页面时运维人员会看到抢单页面
 
 app.run(function ($animate) {
