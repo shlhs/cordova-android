2e82f5da530b0042233d8fcf75d310ce8738e916
yjesefcu <l00164778@sina.com>
2020-03-10 15:51:25 +0800

    宝泉

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -4,7 +4,7 @@
  */
 var app = angular.module('myApp', ['ngAnimate', 'ui.router', 'ui.router.state.events']);
 var loginExpireCheckEnable = false;       // 是否检查鉴权过期
-var defaultPlatIpAddr = "";     // 平台默认ip，格式为：http://118.190.51.135
+var defaultPlatIpAddr = "http://47.105.134.180";     // 宝泉
 var defaultImgThumbHost = "";     // 如果为空则与 host一样
 var gShowEnergyPage = false;     // 是否显示能效页面，不显示能效页面时运维人员会看到抢单页面
 
